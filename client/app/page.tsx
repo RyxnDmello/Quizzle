9037bdd54b4f05aeda239c7495445b1d5889d022
RyxnDmello <ryanndmello10@gmail.com>
2024-10-08 11:41:53 +0530

    Add: Home Page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,30 @@
+import Logo from "@components/Common/Logo";
+import Tile from "@components/Home/Tile";
+
+import Creator from "@public/home/creator.png";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main id="home">
+      <section>
+        <Logo />
+
+        <h1>Who Are <span>You?</span></h1>
+
+        <div>
+          <Tile
+            title="Creator"
+            description="I want to create quizzes."
+            image={Creator}
+          />
+
+          <Tile
+            title="Challenger"
+            description="I want to take part in quizzes."
+            image={Creator}
+          />
+        </div>
+      </section>
+    </main>
+  );
 }
---
+++ client/components/Home/Tile.tsx
@@ -0,0 +1,26 @@
+import { StaticImageData } from "next/image";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./Tile.module.scss";
+
+interface TileProps {
+  image: StaticImageData;
+  title: string;
+  description: string;
+}
+
+export default function Tile({ image, title, description }: TileProps) {
+  return (
+    <Link href={"/register"} className={styles.tile} role="link">
+      <Image src={image} width={10000} height={10000} alt="creator" />
+
+      <div>
+        <h4>{title}</h4>
+        <p>{description}</p>
+      </div>
+
+      <span></span>
+    </Link>
+  );
+}
